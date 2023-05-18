b26d9419031a5b29180fad401d4ed80b1eb9ec44
Максим Логвинов <maksutor@yandex.ru>
2023-05-18 16:05:40 +0300

    Minor fixes

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -4,6 +4,7 @@ from .models import Ingredient, Recipe
 
 
 class IngredientFilter(FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Ingredient
         fields = ['name']
